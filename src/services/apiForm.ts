0f5f351b73ccc8643c148114de45a3892997c0d1
Eric <ericsouza94895@gmail.com>
2024-12-08 17:36:43 -0300

    feat: adicionado mensagem de erro pelo console caso o token do usuário tenha expirado ou seja inválido

--- src/services/apiForm.ts
+++ src/services/apiForm.ts
@@ -11,30 +11,32 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const authToken  = Cookies.get("token");
+  const authToken = Cookies.get("token");
 
   if (!config.headers) {
-      config.headers = {};
+    config.headers = {};
   }
 
   if (authToken) {
-      config.headers.Authorization = `Bearer ${authToken}`;
+    config.headers.Authorization = `Bearer ${authToken}`;
   }
 
   return config;
 }, (error) => {
   console.error('Erro ao configurar o header de autorização:', error);
-
   return Promise.reject(error);
 });
 
-// api.interceptors.response.use(
-//     (response) => response,
-//     (error) => {
-//       console.error('Erro na requisição:', error.response || error.message);
-//       return Promise.reject(error);
-//     }
-//   );
-  
+api.interceptors.response.use((response) => {
+  return response; 
+}, (error) => {
+  if (error.response && error.response.status === 401 || error.response.status === 403) {
+    console.warn("Token expirado ou inválido. Removendo token...");
+    Cookies.remove("token"); 
+    window.location.href = "/login";
+  }
+
+  return Promise.reject(error);
+});
 
 export default api;
