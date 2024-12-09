11400d9c4efe9811e383ecc962fe972b490860ac
Eric <ericsouza94895@gmail.com>
2024-12-09 13:47:11 -0300

    feat: adicionando blocos de try/catch para capturar erros

--- src/services/endpoint/authService.ts
+++ src/services/endpoint/authService.ts
@@ -1,3 +1,4 @@
+import { error } from 'console';
 import api from '../apiForm';
 import Cookies from "js-cookie";
 
@@ -17,24 +18,38 @@ export interface LoginResponseDTO {
 }
 
 export const login = async (authData: AuthDTO): Promise<String> => {
-
+  try{
   const response = await api.post<LoginResponseDTO>('/auth/login', authData);
   Cookies.set("token", response.data.token)
-  
   return "login efetuado com sucesso";
+  }catch(error){
+    throw error;
+  }
 };
 
 export const register = async (registerData: RegisterDTO): Promise<string> => {
+  try{
   const response = await api.post<string>('/auth/register', registerData);
   return response.data;
+  }catch(error){
+    throw error;
+  }
 };
 
 export const verifyUser = async (token: string): Promise<string> => {
+  try{
   const response = await api.get<string>(`/auth/verify?token=${token}`);
   return response.data;
+  }catch(error){
+    throw error;
+  }
 };
 
 export const deleteUser = async (id: string): Promise<string> => {
+  try{
   const response = await api.delete<string>(`/auth/delete/${id}`);
   return response.data;
+  }catch(error){
+    throw error;
+  }
 };
