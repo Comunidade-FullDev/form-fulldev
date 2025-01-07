a588870310db788a55dee4771ebbf34a1289c58f
Eric <ericsouza94895@gmail.com>
2025-01-06 23:38:26 -0300

    feat: adicionando funcionalidades para mudar o nome da área de trabalho e mudar a mensagem de compartilhamento

--- src/components/Dashboard/settings.tsx
+++ src/components/Dashboard/settings.tsx
@@ -24,29 +24,36 @@ import { Switch } from "@/components/ui/switch";
 import { updateDefaultFormSettings } from "@/services/endpoint/form";
 import { Label } from "../ui/label";
 import Modal from "../ui/modal-password";
+import { Textarea } from "../ui/textarea";
 
 const formSchema = z.object({
   workspaceName: z.string().min(2, {
     message: "O nome do espaço de trabalho deve ter pelo menos 2 caracteres.",
   }),
-  language: z.string(),
+  messageShare: z.string(),
   emailNotifications: z.boolean(),
   defaultPrivacy: z.string(),
   password: z.string().optional(),
 });
 
 export default function Settings() {
-  const [isModalOpen, setModalOpen] = React.useState(false);
-  const [password, setPassword] = useState("");
-
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [storedWorkspaceName, setWorkspaceName] = useState(
+    localStorage.getItem("WorkspaceName") || "Meu Espaço de Trabalho"
+  );  const messageShare = localStorage.getItem("sharedMessage") || "Responda o meu formulário acessando o link abaixo: \n"
   const storedPrivacy = localStorage.getItem("defaultPrivacy") || "public";
+
+  const getNotification = () => {
+    const notification = localStorage.getItem("notification");
+    return notification === "true";
+  };
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      workspaceName: "Meu Espaço de Trabalho",
-      language: "pt-BR",
-      emailNotifications: true,
+      workspaceName: storedWorkspaceName,
+      messageShare: messageShare,
+      emailNotifications: getNotification(),
       defaultPrivacy: storedPrivacy,
       password: "",
     },
@@ -54,8 +61,8 @@ export default function Settings() {
 
   useEffect(() => {
     const subscription = form.watch((value) => {
-      const defaultPrivacy = value.defaultPrivacy || "";
-      localStorage.setItem("defaultPrivacy", defaultPrivacy);
+      localStorage.setItem("defaultPrivacy", value.defaultPrivacy || "public");
+      localStorage.setItem("notification", JSON.stringify(value.emailNotifications || true));
     });
 
     return () => {
@@ -80,21 +87,25 @@ export default function Settings() {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-        const standard = form.watch("defaultPrivacy");
-        const sendEmail = form.watch("emailNotifications");
-        console.log(sendEmail)
-        const { password } = values;
+      const standard = form.watch("defaultPrivacy")
+      const sendEmail = form.watch("emailNotifications")
 
-        console.log("Payload enviado:", { defaultPrivacy: standard, password, sendEmail });
+      await updateDefaultFormSettings(standard, values.password || "", sendEmail);
+      alert("Configurações salvas com sucesso!");
 
-        await updateDefaultFormSettings(standard, password || "", sendEmail);
-        console.log("Configurações atualizadas com sucesso:", values);
-        alert("Configurações salvas com sucesso!");
+      localStorage.setItem("defaultPrivacy", standard);
+      localStorage.setItem("notification", JSON.stringify(sendEmail));
+      localStorage.setItem("sharedMessage", values.messageShare)
+
+      if (localStorage.getItem("WorkspaceName") !== values.workspaceName) {
+        localStorage.setItem("WorkspaceName", values.workspaceName);
+        window.location.reload()
+      }
     } catch (error) {
-        console.error("Erro ao salvar configurações:", error);
-        alert("Erro ao salvar as configurações. Tente novamente.");
+      console.error("Erro ao salvar configurações:", error);
+      alert("Erro ao salvar as configurações. Tente novamente.");
     }
-};
+  };
 
 
   return (
@@ -116,7 +127,28 @@ export default function Settings() {
             </FormItem>
           )}
         />
-        
+
+        <FormField
+          control={form.control}
+          name="messageShare"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Mensagem de compartilhamento</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Mensagem de compartilhamento"
+                  {...field}
+                  className="resize-none h-24 w-full"
+                />
+              </FormControl>
+              <FormDescription>
+                Esta é a mensagem de compartilhamento padrão dos formulários.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <FormField
           control={form.control}
           name="emailNotifications"
@@ -167,11 +199,11 @@ export default function Settings() {
 
         <Button type="submit">Salvar Alterações</Button>
       </form>
-      
+
       {isModalOpen && (
         <Modal title="Definir Senha">
           <p>Digite a senha para proteger o formulário:</p>
-          
+
           <div className="space-y-2 w-full">
 
             <Label htmlFor="password">Senha</Label>
@@ -181,7 +213,7 @@ export default function Settings() {
               placeholder="Insira a senha do seu formulário aqui"
               className="w-full text-white"
               value={form.watch("password")}
-            onChange={(e) => form.setValue("password", e.target.value)}
+              onChange={(e) => form.setValue("password", e.target.value)}
             />
           </div>
           <div className="mt-4 flex justify-end space-x-2">
@@ -194,8 +226,8 @@ export default function Settings() {
             </Button>
 
             <Button onClick={() => setModalOpen(false)} variant="outline" className="bg-red-600 hover:bg-red-900 text-white">
-            Cancelar
-          </Button>
+              Cancelar
+            </Button>
           </div>
         </Modal>
       )}
